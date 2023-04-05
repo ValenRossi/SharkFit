a18d8bd87c0d631acc63e14c7371f31270f16a41
Valentin <valentin.rossi1@hotmail.com>
2023-04-04 22:38:04 -0300

    preentrega2

--- proyecto.js
+++ proyecto.js
@@ -19,14 +19,14 @@ if (remeras == 'remera oversize WheyFit'){
 }else if(remeras == 'remera azul WheyFit'){
     console.log('remera azul WheyFit ... $ 4000');
 }else{
-    console.log('Remera sin stock')
+    console.log('Remera sin stock');
 }
 
 //funcion
 let precio1=4500;
 let precio2=4000;
 let precio3=4000;
-let total=precio1+precio2+precio3;s
+let total=precio1+precio2+precio3;
 
 function calcularTotalConIva(totalCompra){
     let totalConIva=totalCompra * 1.21;
@@ -35,7 +35,8 @@ function calcularTotalConIva(totalCompra){
 
 calcularTotalConIva(total);
 
-let mancuernas = prompt('Ingresa la mancuerna y tse dire el precio (s para salir)');
+
+let mancuernas = prompt('Ingresa la mancuerna y te dire el precio (s para salir)');
 while (mancuernas!='s'){
     switch(mancuernas){
         case '5kg':
@@ -61,3 +62,23 @@ while (mancuernas!='s'){
 }
 
 
+class Producto{
+    constructor(marca, producto, precio){
+        this.marca = marca;
+        this.producto = producto;
+        this.precio = precio;
+    this.mostrarSuplementos = function(){
+        alert(this.marca + ' ' +this.producto + ' ' + this.precio);
+        }
+    }
+}
+
+const p1 = new Producto('Star Nutricion','Whey Protein 3kg','$16900');
+const p2 = new Producto('Star Nutricion','Mutant Mass 5kg','$11900');
+const p3 = new Producto('Star Nutricion','Creatina 300g','$8800');
+const p4 = new Producto('ENA','Whey Protein 1kg','$5100');
+const p5 = new Producto('ENA','UltraMass 3kg','$7000');
+const p6 = new Producto('ENA','Creatina sin sabor 60 serv','$7200');
+
+const productos = [p1,p2,p3,p4,p5,p6];
+console.log(productos);
